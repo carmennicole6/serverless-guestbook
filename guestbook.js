a1ea777bf9ea40db4b189d3a63c419dc233eabaf
carmennicole6 <118977268+carmennicole6@users.noreply.github.com>
2023-03-26 15:11:26 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = 'https://eu-gb.functions.appdomain.cloud/api/v1/web/766eb1d5-45d0-4626-9005-3678649a0edf/guestbook/read-guestbook-entries-sequence';
+const apiUrl = 'https://eu-gb.functions.appdomain.cloud/api/v1/web/766eb1d5-45d0-4626-9005-3678649a0edf/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save-guestbook-entry-sequence.json`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
